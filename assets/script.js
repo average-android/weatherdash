c944714881598f422b901519d97f5fd37fa484de
Mitchel B <staysaucy555@gmail.com>
2023-03-08 23:17:51 -0500

    added clock and header, reorganized file layout

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var apiKey ; "278f7c5fc519547f73f9996c475c3d92";
+
+
+var citySearch = document.querySelector("#searchCity");
+
+function clock() {
+    var timeEl = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    $("#time").text("Local Time: " + timeEl);
+    console.log(timeEl);
+}
+setInterval(clock, 1000);
--- script.js
+++
@@ -1,5 +0,0 @@
-var apiKey ; "278f7c5fc519547f73f9996c475c3d92";
-
-
-var citySearch = document.querySelector("#searchCity");
-
